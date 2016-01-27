18542d46e42cce878cededfc11d2db7e1502a111
stryku <stryku2393@gmail.com>
2016-01-27 13:41:05 +0100

    cleaned RequestHandler

--- servers/pc/src/RequestHandler.hpp
+++ servers/pc/src/RequestHandler.hpp
@@ -32,11 +32,58 @@ namespace HD
             }
 
         public:
-            auto handleFileRequest( const pt::ptree &tree )
+            auto handleFileRequest( const pt::ptree &tree ) const
             {
                 auto relativePath = tree.get_child( "request.path" ).get_value( "" );
 
                 return FileResponse( relativePath ).toZmqMessage();
+            }
+
+            auto handleListRequest() const
+            {
+                return  FilesListResponse( Filesystem::getMainFolderPath() ).toZmqMessage();
+            }
+
+            auto handleNewFile( const pt::ptree &tree ) const
+            {
+                auto relativePathToFile = tree.get_child( "request.path" ).get_value( "" );
+                auto fullPathToFileToCreate = Filesystem::getMainFolderPath() + relativePathToFile;
+                auto base64File = tree.get_child( "msg.file" ).get_value( "" );
+
+                Filesystem::FilesystemAffect::createFileFromBase64( fullPathToFileToCreate,
+                                                                    base64File );
+
+                return std::make_shared<zmq::message_t>(); //TODO
+            }
+
+            auto handleNewDir( const pt::ptree &tree ) const
+            {
+                auto relativePathToDir = tree.get_child( "request.path" ).get_value( "" );
+                auto fullPathToDirToCreate = Filesystem::getMainFolderPath() + relativePathToDir;
+
+                Filesystem::FilesystemAffect::createDirectory( fullPathToDirToCreate );
+
+                return std::make_shared<zmq::message_t>(); //TODO
+            }
+
+            auto handleRemoveFile( const pt::ptree &tree ) const
+            {
+                auto relativePathToFile = tree.get_child( "request.path" ).get_value( "" );
+                auto fullPathToFileToDelete = Filesystem::getMainFolderPath() + relativePathToFile;
+
+                Filesystem::FilesystemAffect::deleteFile( fullPathToFileToDelete );// todo
+
+                return std::make_shared<zmq::message_t>(); //TODO
+            }
+
+            auto handleRemoveDir( const pt::ptree &tree ) const
+            {
+                auto relativePathToDir = tree.get_child( "request.path" ).get_value( "" );
+                auto fullPathToDirToDelete = Filesystem::getMainFolderPath() + relativePathToDir;
+
+                Filesystem::FilesystemAffect::deleteDirectory( fullPathToDirToDelete );// todo
+
+                return std::make_shared<zmq::message_t>(); //TODO
             }
 
             auto handle( const zmq::message_t &msg )
@@ -51,28 +98,27 @@ namespace HD
                 {
                     case MessageType::LIST_REQ: 
                         LOG( "LIST_REQ" );
-                        return  FilesListResponse( Filesystem::getMainFolderPath() ).toZmqMessage();//todo
+                        return handleListRequest();
 
                     case MessageType::FILE_REQ:
                         LOG( "FILE_REQ" );
                         return  handleFileRequest( tree );
 
                     case MessageType::NEW_FILE:
-                        Filesystem::FilesystemAffect::createFileFromBase64( Filesystem::getMainFolderPath() + tree.get_child( "request.path" ).get_value( "" ),// todo
-                                                                            tree.get_child( "msg.file" ).get_value( "" ) );
-                    break;
+                        LOG( "NEW_FILE" );
+                        return handleNewFile( tree );
 
                     case MessageType::NEW_DIR:
-                    Filesystem::FilesystemAffect::createDirectory( Filesystem::getMainFolderPath() + tree.get_child( "request.path" ).get_value( "" ) );// todo
-                    break;
+                        LOG( "NEW_DIR" );
+                        return handleNewDir( tree );
 
                     case MessageType::REMOVE_FILE:
-                        std::experimental::filesystem::remove( Filesystem::getMainFolderPath() + tree.get_child( "request.path" ).get_value( "" ) );// todo
-                    break;
+                        LOG( "REMOVE_FILE" );
+                        return handleRemoveFile( tree );
 
                     case MessageType::REMOVE_DIR:
-                        std::experimental::filesystem::remove_all( Filesystem::getMainFolderPath() + tree.get_child( "request.path" ).get_value( "" ) );// todo
-                    break;
+                        LOG( "REMOVE_DIR" );
+                        return handleRemoveDir( tree );
 
                     default:
                     break;
