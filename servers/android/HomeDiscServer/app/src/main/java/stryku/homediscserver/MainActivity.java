86bb669c2479cb02e3d352205affaee1b3abb304
stryku <stryku2393@gmail.com>
2016-02-14 00:28:59 +0100

    created basic view

--- servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/MainActivity.java
+++ servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/MainActivity.java
@@ -4,14 +4,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.zeromq.ZContext;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static int MAX_EVENTS = 1000;
+
+    ArrayList<String> events = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                events);
+
+        ListView lv = (ListView)findViewById(R.id.listViewLatestEvents);
+        lv.setAdapter( adapter );
     }
 
     @Override
@@ -34,5 +52,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addEventToList( String eventDescription )
+    {
+        if( events.size() == MAX_EVENTS )
+            events.remove( events.size() - 1 );
+
+        events.add( 0, eventDescription );
     }
 }
