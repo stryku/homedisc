804dbc9045d08c8411d7bb2f99180239e5dc034a
stryku <stryku2393@gmail.com>
2016-02-14 01:09:14 +0100

    added display ip and port

--- servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/MainActivity.java
+++ servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/MainActivity.java
@@ -1,16 +1,18 @@
 package stryku.homediscserver;
 
-import android.support.v7.app.ActionBarActivity;
+import android.media.audiofx.BassBoost;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.format.Formatter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import org.zeromq.ZContext;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -30,6 +32,34 @@ public class MainActivity extends ActionBarActivity {
 
         ListView lv = (ListView)findViewById(R.id.listViewLatestEvents);
         lv.setAdapter( adapter );
+
+        setIpAndPort();
+    }
+
+    public String getLocalIpAddress()
+    {
+        try
+        {
+            WifiManager wifiMgr = (WifiManager) getSystemService(WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+            int ip = wifiInfo.getIpAddress();
+
+            return Formatter.formatIpAddress(ip);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private void setIpAndPort()
+    {
+        String ip = getLocalIpAddress();
+        int port = Settings.getPort();
+        TextView tv = (TextView) findViewById(R.id.textViewIpAndPort);
+
+        tv.setText( String.format( "%s:%d", ip, port ) );
     }
 
     @Override
