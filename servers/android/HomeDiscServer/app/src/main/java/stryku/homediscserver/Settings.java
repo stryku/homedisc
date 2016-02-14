775e5d8c966131ee08cbb3508e56637ce3801755
stryku <stryku2393@gmail.com>
2016-02-14 01:09:31 +0100

    created Settings

---
+++ servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/Settings.java
@@ -0,0 +1,13 @@
+package stryku.homediscserver;
+
+/**
+ * Created by stryku on 14.02.16.
+ */
+public class Settings {
+
+    static public int getPort() {
+        return port;
+    }
+
+    static private int port = 2393;
+}
