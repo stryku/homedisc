b5ac5d504641fd647b2eb119ef12d0c19a6b03d8
stryku <stryku2393@gmail.com>
2016-02-19 23:15:48 +0100

    added setting HomeDiscServer on external directory

--- servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/Settings.java
+++ servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/Settings.java
@@ -1,11 +1,13 @@
 package stryku.homediscserver;
 
+import android.os.Environment;
+
 /**
  * Created by stryku on 14.02.16.
  */
 public class Settings {
     static private int port = 2393;
-    static private String mainFolderPath = "";
+    static private String mainFolderPath = Environment.getExternalStorageDirectory().getPath() + "/HomeDiscServer";
 
     static public int getPort() {
         return port;
