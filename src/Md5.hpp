054f52bbfae05dfdbfa85b9542cceae35432c129
stryku <stryku2393@gmail.com>
2016-02-22 16:16:26 +0100

    improved md5 wrapper

--- src/Md5.hpp
+++ src/Md5.hpp
@@ -3,11 +3,13 @@
 #include <md5/md5.h>
 
 #include <memory>
-#include <istream>
+#include <stdexcept>
+#include <fstream>
+#include <string>
 
 namespace Md5
 {
-    std::string hash( std::istream &in )
+    std::string hashStream( std::istream &in )
     {
         static const size_t BufferSize = 1024*1024;
 
@@ -25,4 +27,15 @@ namespace Md5
 
         return md5.getHash();
     }
+
+    std::string hashFile( const std::string &filename )
+    {
+        std::ifstream in( filename, std::ios::binary );
+
+        if( !in.good() )
+            throw std::runtime_error( "Can't open file: " + filename );
+
+        return hashStream( in );
+    }
+
 };
