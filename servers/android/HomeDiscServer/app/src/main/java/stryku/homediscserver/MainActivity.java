15c848c4b9a03ac10a460cd851a2ce9982c9dd02
stryku <stryku2393@gmail.com>
2016-02-29 15:22:32 +0100

    removed last todo

--- servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/MainActivity.java
+++ servers/android/HomeDiscServer/app/src/main/java/stryku/homediscserver/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends ActionBarActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {//TODO move server to another method, thread joining
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
